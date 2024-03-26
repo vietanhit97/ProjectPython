4030ef2dd828e6edffc0d0deba2f83c36434326d
vietanhit97 <vietanhit97@users.noreply.github.com>
2024-03-26 02:01:15 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="vietanhit97"
+original_name="projectpython"
+original_urlname="ProjectPython"
+original_description="Awesome projectpython created by vietanhit97"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv projectpython $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ projectpython/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for projectpython."""
+
+from projectpython.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
